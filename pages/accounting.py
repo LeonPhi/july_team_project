9ad17b4b709166f55c0f215cfd8b32abff5b9a30
LeonPhi <leon1618phi@gmail.com>
2025-07-27 22:25:11 +0800

    Delete pages directory

--- pages/accounting.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from utils.sidebar import render_sidebar
-
-st.title("Accounting 記帳")
-
-render_sidebar()
--- pages/card.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from utils.sidebar import render_sidebar
-
-st.title("Greeting Card 賀卡")
-
-render_sidebar()
