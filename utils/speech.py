6f36a6180ce5bc4a37bc74335972ce7ddab6a8fc
LeonPhi <leon1618phi@gmail.com>
2025-07-31 23:31:30 +0800

    Update speech.py

--- utils/speech.py
+++ utils/speech.py
@@ -1,5 +1,6 @@
 from google import genai
 from google.genai import types
+from google.genai.errors import ClientError
 import streamlit as st 
 
 def speech_to_text(key: int):
@@ -14,37 +15,43 @@ def speech_to_text(key: int):
     audio = st.audio_input("語音輸入", key=key)
 
     def generate_language():
-        response = client.models.generate_content(
-            model="gemini-2.5-flash", 
-            contents=[
-                """Please return what language this audio speech is in.
-                    Example: AUDIO- I am 10 years old. RETURN- English
-                    Example: AUDIO- 我現在十歲。 RETURN- Traditional Chinese ***(DEFAULT CHINESE: Traditional Chinese)***
-                    Example: AUDIO- Ich bin sechzehn Jahre alt. RETURN- German""",
-                types.Part.from_bytes(
-                    data=audio,
-                    mime_type='audio/mp3'
-                )
-            ],
-        )
+        try:
+            response = client.models.generate_content(
+                model="gemini-2.5-flash", 
+                contents=[
+                    """Please return what language this audio speech is in.
+                        Example: AUDIO- I am 10 years old. RETURN- English
+                        Example: AUDIO- 我現在十歲。 RETURN- Traditional Chinese ***(DEFAULT CHINESE: Traditional Chinese)***
+                        Example: AUDIO- Ich bin sechzehn Jahre alt. RETURN- German""",
+                    types.Part.from_bytes(
+                        data=audio,
+                        mime_type='audio/mp3'
+                    )
+                ],
+            )
+        except ClientError:
+            return False
         return response.text
 
     def generate(language):
-        response = client.models.generate_content(
-            model="gemini-2.5-flash", 
-            contents=[
-#               """Please create a transcript of the speech. Do not add any timestamps, just plain words.
-#                   請建立語音的文字記錄。請勿添加任何時間戳，只需要說出的文字即可。
-#                   Bitte erstellen Sie ein Transkript der Rede. Fügen Sie keine Zeitstempel hinzu, nur die einfachen Wörter.""", 
-                f"""Please create a transcript of the speech. The speech is in {language}. 
-                    Do not add any timestamps, just plain words.""",
-                types.Part.from_bytes(
-                    data=audio,
-                    mime_type='audio/mp3'
-                )
-            ],
-            config=config
-        )
+        try:
+            response = client.models.generate_content(
+                model="gemini-2.5-flash", 
+                contents=[
+#                   """Please create a transcript of the speech. Do not add any timestamps, just plain words.
+#                       請建立語音的文字記錄。請勿添加任何時間戳，只需要說出的文字即可。
+#                       Bitte erstellen Sie ein Transkript der Rede. Fügen Sie keine Zeitstempel hinzu, nur die einfachen Wörter.""", 
+                    f"""Please create a transcript of the speech. The speech is in {language}. 
+                        Do not add any timestamps, just plain words.""",
+                    types.Part.from_bytes(
+                        data=audio,
+                        mime_type='audio/mp3'
+                    )
+                ],
+                config=config
+            )
+        except ClientError:
+            return False
         return response.text
 
     if audio:
